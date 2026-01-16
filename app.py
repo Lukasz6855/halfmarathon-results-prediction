7f09531ea0396f147cb59400023d928124d436a0
Lukasz <lukasz6855@op.pl>
2026-01-16 23:57:22 +0100

    popraw wrapper - zmień working directory na APP/

--- app.py
+++ app.py
@@ -5,8 +5,17 @@ Uruchamia aplikację z folderu APP/
 import sys
 import os
 
-# Dodaj folder APP do ścieżki
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'APP'))
+# Pobierz ścieżkę do głównego folderu
+root_dir = os.path.dirname(os.path.abspath(__file__))
+app_dir = os.path.join(root_dir, 'APP')
 
-# Importuj główną aplikację
-from APP.app import *
+# Zmień working directory na APP/
+os.chdir(app_dir)
+
+# Dodaj APP do ścieżki Pythona
+sys.path.insert(0, app_dir)
+
+# Teraz załaduj i uruchom aplikację
+with open(os.path.join(app_dir, 'app.py'), 'r', encoding='utf-8') as f:
+    code = f.read()
+    exec(code)
